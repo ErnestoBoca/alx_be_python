12ed9ef15317650e73c53fc8b62ac588a8454e2b
Ernesto Boca <boca.ernesto13@gmail.com>
2025-06-12 09:30:40 +0200

    Addem try catch

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -10,10 +10,16 @@ def convert_to_fahrenheit(celsius):
 
 
 if __name__ == "__main__":
-    temperature = float(input("Enter the temperature to convert: "))
-    type_t = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
-
-    if type_t == "F":
-        print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
-    else:
-        print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
+    try:
+        temperature = float(input("Enter the temperature to convert: "))
+        type_t = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+    
+        temperature = float(temperature)            
+        if type_t == "F":
+            print(f"{temperature}°F is {convert_to_celsius(temperature)}°C")
+        elif type_t == "C":
+            print(f"{temperature}°C is {convert_to_fahrenheit(temperature)}°F")
+        else:
+            print("Invalid unit. Please enter a numeric value.")
+    except ValueError:
+        print("Invalid temperature. Please enter a numeric value.")
